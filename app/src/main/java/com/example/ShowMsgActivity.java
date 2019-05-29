355f8762151aa7a106e8f1b1aa35603dc7f8d577
hayoungjung69 <hayoungjung69@gmail.com>
2019-05-30 05:02:31 +0900

    백그라운드에서 팝업뜨기

---
+++ app/src/main/java/com/example/ShowMsgActivity.java
@@ -0,0 +1,30 @@
+package com.example;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.TextView;
+
+import com.example.programmingknowledge.mybalance_v11.R;
+
+public class ShowMsgActivity extends AppCompatActivity {
+
+    TextView textView_popup;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_BLUR_BEHIND,
+                WindowManager.LayoutParams.FLAG_BLUR_BEHIND);
+
+        textView_popup = (TextView)findViewById(R.id.textView_popup);
+
+    }
+
+
+
+
+
+}
